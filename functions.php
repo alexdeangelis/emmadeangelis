ea94e1e58f9856f6a36d8040404a0f140f484e33
Alex De Angelis <alexdeangelis86@googlemail.com>
2022-10-10 22:41:52 +0100

    updating enqueue function file path

--- functions.php
+++ functions.php
@@ -8,17 +8,13 @@ add_action( 'init', 'register_menus' );
 
 //Enqueue CSS & JS
 
-function your_scripts() {
-	/*Bootstrap 5*/
-	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' );
+function emma_scripts() {
 	/*Font Awesome*/
 	wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v6.1.2/css/all.css' );
 	/*Basic Styles*/
-	wp_enqueue_style( 'styles', get_stylesheet_uri(), array(), '1.0.0' );
-	/*Bootstrap JS*/
-	wp_enqueue_script('bootstrap-js-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array(), null, true);
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/dist/style.css', array(), '1.0.4' );
 }
-add_action( 'wp_enqueue_scripts', 'your_scripts' );
+add_action( 'wp_enqueue_scripts', 'emma_scripts' );
 
 /*
  * Let WordPress manage the document title.
