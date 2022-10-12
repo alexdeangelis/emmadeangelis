a8c18bfc6a13ef502711860018afe7352fee7343
Alex De Angelis <alexdeangelis86@googlemail.com>
2022-10-12 21:24:10 +0100

    terser plugin & minify js

--- dist/theme.bundle.js
+++ dist/theme.bundle.js
@@ -1,12 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-var hamburger = document.querySelector('.header__navbar__hamburger');
-var hamburgerSpans = hamburger.getElementsByTagName("span");
-var mobileMenu = document.querySelector('.header__mobile-menu');
-hamburger.addEventListener('click', function (e) {
-  this.classList.contains('open') ? this.classList.remove('open') : this.classList.add('open');
-  mobileMenu.classList.contains('open') ? mobileMenu.classList.remove('open') : mobileMenu.classList.add('open');
-});
-/******/ })()
-;
+(()=>{var e=document.querySelector(".header__navbar__hamburger"),s=(e.getElementsByTagName("span"),document.querySelector(".header__mobile-menu"));e.addEventListener("click",(function(e){this.classList.contains("open")?this.classList.remove("open"):this.classList.add("open"),s.classList.contains("open")?s.classList.remove("open"):s.classList.add("open")}))})();
 //# sourceMappingURL=theme.bundle.js.map
